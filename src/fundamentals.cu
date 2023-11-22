f9d42a935f4edf74ef83d5a792feb96957a0e99e
Ziqi Fan <ziqi.francis.fan@gmail.com>
2023-11-22 14:01:08 -0800

    Created a simple cmake build system.

---
+++ src/fundamentals.cu
@@ -0,0 +1,22 @@
+#include "fundamentals.cuh"
+#include <iostream>
+
+__global__ void addVec()
+{
+    if (blockIdx.x == 0 && threadIdx.x == 0)
+    {
+        printf("test in %s\n", __FUNCTION__);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+#ifdef DEBUG
+    std::cout << "This is a debug version project." << std::endl;
+#else
+    std::cout << "This is a release version project." << std::endl;
+#endif
+    std::cout << "This is a project for image processing by Ziqi." << std::endl;
+    addVec<<<1,32,0,0>>>();
+    cudaDeviceSynchronize();
+}
